3f75d602d715b75406dbeec4a994fcba58f6a11b
HanieGolrokh <golrokh.hanie@gmail.com>
2023-11-02 18:50:40 +0100

    Edit

---
+++ LearningJava/src/HelloWorld.java
@@ -0,0 +1,15 @@
+//public class HelloWorld {
+//    public HelloWorld() {
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println("Hello, World!");
+//    }
+//}
+//----------------------------------------------
+public class HelloWorld {
+    public static void main(String[] args) {
+        var message = "Hello, World!";
+        System.out.println(message);
+    }
+}
